8623967a31319239bb58dfcd41dbd2092d4985f0
wubnagcdu <ejd1211657@163.com>
2021-04-09 10:04:50 +0800

    remove lib

--- lib/src/main/cpp/ih_http_client.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#define MAXLINE 1024
--- lib/src/main/cpp/ih_ip.h
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-bool get_public_ip(char *);
-bool get_local_ip(char *);
